b70689c2e419055f967f897d8a375584f4a9a783
yarrobong <oneboardsholi@gmail.com>
2023-09-12 00:00:05 +0500

    Добавьте файлы проекта.

---
+++ PR3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число: ");
+        string inputString = Console.ReadLine();
+
+        int number;
+        if (int.TryParse(inputString, out number))
+        {
+            Console.WriteLine($"The next number for the number {number} is {number + 1}.");
+            Console.WriteLine($"The previous number for the number {number} is {number - 1}.");
+        }
+        else
+        {
+            Console.WriteLine("Это не число");
+        }
+    }
+}
